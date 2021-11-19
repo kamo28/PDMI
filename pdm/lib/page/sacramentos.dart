2ff7574653b0d1148ba18093ff7c917301255a30
Fernando_Corrales <fjc1997v@gmail.com>
2021-11-18 23:53:45 -0600

    botones sacramentos

--- pdm/lib/page/sacramentos.dart
+++ pdm/lib/page/sacramentos.dart
@@ -1,5 +1,3 @@
-import 'dart:js_util';
-
 import 'package:flutter/material.dart';
 import 'package:pdm/inicio.dart';
 import 'package:pdm/widget/navigation_drawer_widget.dart';
@@ -39,14 +37,76 @@ class sacramentos extends StatelessWidget {
                             child: RichText(
                                 text: TextSpan(children: [
                               TextSpan(
-                                  text: "Sacramentos    ",
+                                  text: "Bautismo    ",
+                                  style: TextStyle(
+                                      fontSize: 34, color: Colors.black)),
+                              WidgetSpan(child: Icon(Icons.pin_drop, size: 70))
+                            ])),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(10),
+                        height: 200.0,
+                        width: 340,
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(
+                                  color: Color.fromRGBO(0, 160, 227, 1))),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => sacramentos()));
+                          },
+                          padding: EdgeInsets.all(10.0),
+                          color: Colors.white38,
+                          textColor: Colors.black,
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: RichText(
+                                text: TextSpan(children: [
+                              TextSpan(
+                                  text: "Confirmación    ",
+                                  style: TextStyle(
+                                      fontSize: 34, color: Colors.black)),
+                              WidgetSpan(child: Icon(Icons.check, size: 70))
+                            ])),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(10),
+                        height: 200.0,
+                        width: 340,
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(
+                                  color: Color.fromRGBO(0, 160, 227, 1))),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => sacramentos()));
+                          },
+                          padding: EdgeInsets.all(10.0),
+                          color: Colors.white38,
+                          textColor: Colors.black,
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: RichText(
+                                text: TextSpan(children: [
+                              TextSpan(
+                                  text: "Primera comunión        ",
                                   style: TextStyle(
                                       fontSize: 34, color: Colors.black)),
                               WidgetSpan(child: Icon(Icons.book, size: 70))
                             ])),
                           ),
                         ),
-                      )
+                      ),
                     ],
                   ),
                 )),
