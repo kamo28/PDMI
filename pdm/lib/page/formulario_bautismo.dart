83a210e29a9ef8dbffeadfc9c813ec4265e0047a
KilloconQ <47192168+KilloconQ@users.noreply.github.com>
2021-11-29 17:37:36 -0600

    formulario de bautismo feo

--- pdm/lib/page/formulario_bautismo.dart
+++ pdm/lib/page/formulario_bautismo.dart
@@ -3,18 +3,48 @@ import 'package:flutter/material.dart';
 class formulario_bautismo extends StatelessWidget {
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
-          title: Text("Solicitud de Sacramentos"),
+          title: Text("Bautismo"),
         ),
         body: Builder(
             builder: (context) => Center(
                   child: new Column(
                     children: <Widget>[
-                      Text('prueba'),
+                      Text('Fecha de bautismo'),
                       TextField(
                         decoration: const InputDecoration(
                           border: OutlineInputBorder(),
                         ),
-                      )
+                      ),
+                      Text('Ministro'),
+                      TextField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      Text('Nombre del beneficiado'),
+                      TextField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      Text('Nombre del beneficiado'),
+                      TextField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      Text('Diocesis'),
+                      TextField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      Text('Parroquia'),
+                      TextField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
                     ],
                   ),
                 )),
